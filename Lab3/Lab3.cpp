ff91a41c1e2085d6aa70c6edef8e00512261a440
Никита <@DESKTOP-0N74OFC>
2023-10-15 18:23:20 +0700

    Добавьте файлы проекта.

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<clocale>
+using namespace std;
+int main() {	
+	setlocale(LC_ALL, "RUSSIAN");
+	int UserNum;
+	cin >> UserNum;
+	if ((UserNum / 100 != (UserNum / 10 % 10)) and (UserNum % 10 != UserNum / 100) and ((UserNum / 10 % 10) != UserNum % 10)) {
+		cout << "Все числа различны\n";
+	}
+	else {
+		cout << "Числа не различны\n";
+	}
+	return 0;
+}
